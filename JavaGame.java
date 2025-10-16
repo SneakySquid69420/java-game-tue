fe64ce39ba57d1fd5558a86844d188b637ff97c5
Meer <l.v.d.meer@student.tue.nl>
2025-10-16 11:34:22 +0200

    swing changes

---
+++ JavaGame.java
@@ -0,0 +1,7 @@
+
+public class Java_game {
+
+
+    public static void main(String[] args) {
+    }
+}
---
+++ JavaServer.java
@@ -0,0 +1,7 @@
+
+public class Java_server {
+
+
+    public static void main(String[] args) {
+    }
+}
