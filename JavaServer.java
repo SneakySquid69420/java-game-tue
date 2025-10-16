1d1c260e963478f451806775f9409400a7bd6f11
Michielse <d.j.p.m.michielse@student.tue.nl>
2025-10-16 11:36:04 +0200

    main game logic

---
+++ JavaServer.java
@@ -0,0 +1,17 @@
+/**
+ * The server logic.
+ * 
+ * @author Lago van der Meer
+ * @id 2324717
+ * @author Daan Michielse
+ * @id 2318644
+ */
+public class JavaServer {
+    void run() {
+    
+    }
+    
+    public static void main(String[] args) {
+        new JavaServer().run();
+    }
+}
---
+++ JavaSwing.java
@@ -0,0 +1,17 @@
+/**
+ * The swing logic.
+ * 
+ * @author Lago van der Meer
+ * @id 2324717
+ * @author Daan Michielse
+ * @id 2318644
+ */
+public class JavaSwing {
+    void run() {
+    
+    }
+    
+    public static void main(String[] args) {
+        new JavaSwing().run();
+    }
+}
